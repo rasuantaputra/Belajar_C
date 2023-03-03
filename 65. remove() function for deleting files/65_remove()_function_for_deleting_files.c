bbc69be2f4f862abdc393f7ba78a63b7a575b4ca
rasuantaputra <you@example.com>
2023-03-03 16:51:53 +0700

    .vscode/settings.json

---
+++ 65. remove() function for deleting files/65_remove()_function_for_deleting_files.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(int argc, char const *argv[])
+{
+    char file_name[100];
+
+    printf("File: ");
+    scanf("%s", file_name);
+
+    // if success return -1 if unsuccess return 0
+    if (remove(file_name) != 0)
+    {
+        
+        fprintf(stderr, "Errno: %d\n", errno);
+        perror("Ini dia pesan error nya");
+    }else printf("%s deleted. \n", file_name);
+    
+
+
+
+    return 0;
+}
