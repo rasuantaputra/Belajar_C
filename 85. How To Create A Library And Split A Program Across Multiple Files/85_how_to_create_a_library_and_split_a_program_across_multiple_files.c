4189bb5b9f17ad4b6b7c8c7e1bdd7383346e9ccd
rasuantaputra <you@example.com>
2023-03-24 08:13:00 +0700

    85. How To Create A Library And Split A Program Across Multiple Files

--- 85. How To Create A Library And Split A Program Across Multiple Files/85_how_to_create_a_library_and_split_a_program_across_multiple_files.c
+++ 85. How To Create A Library And Split A Program Across Multiple Files/85_how_to_create_a_library_and_split_a_program_across_multiple_files.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include "library.h"
 
-/**
- * @note kalau dipisah gatau kenapa jadinya gagal
- */
-
 int main(int argc, char const *argv[])
 {
     int x = 6;
--- 85. How To Create A Library And Split A Program Across Multiple Files/library.c
+++ 85. How To Create A Library And Split A Program Across Multiple Files/library.c
@@ -1,5 +1,7 @@
 // this file fore defenitioning function in header file
 // inside this library, we can include another library
+
+// its ok if we dont use "#include<stdio.h>"
 // #include<stdio.h>
 int add(int a, int b)
 {
