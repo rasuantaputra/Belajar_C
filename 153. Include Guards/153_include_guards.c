6e6179f10971d1ff71b20017db76243d2962c1b6
rasuantaputra <you@example.com>
2023-03-27 11:33:17 +0700

    153. Include Guards

---
+++ 153. Include Guards/153_include_guards.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "course.h"
+#include "student.h"
+
+/**
+ * @brief include gurad used to prevent defined header file or "including" file  twice
+ * 
+ * @note there is two ways to write include guard
+ * @note "student.h" using 1st way
+ * @note "content.h" using 2nd way
+ */
+
+int main()
+{
+    
+    
+    return 0;
+}
---
+++ 153. Include Guards/student.h
@@ -0,0 +1,13 @@
+// becareful!! macro name we use must be unique and this purpose
+#ifndef STUDENT_H
+#define STUDENT_H
+
+// #pragma once
+
+typedef struct
+{
+    char name[100];
+    int id;
+} Student;
+
+#endif
